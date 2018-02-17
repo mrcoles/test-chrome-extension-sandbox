a2a43b797f7745b8adff2d425a8bcbc9d7a2da72
Peter Coles <peter@mrcoles.com>
2018-02-17 15:48:19 -0500

    simplify test to just use a timeout

--- popup.js
+++ popup.js
@@ -8,6 +8,4 @@ function main() {
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus auctor accumsan tincidunt. Fusce odio dui, ultricies sit amet ipsum a, ultrices ultrices ligula. In hac habitasse platea dictumst. Aliquam ullamcorper tempor ligula, in feugiat elit porta sed. Cras quis tincidunt lorem, vel pulvinar diam. Sed justo nulla, sodales accumsan rutrum vel, dictum in libero. Morbi pulvinar sapien nulla, eu mattis arcu mollis in. In vehicula lectus mi, sit amet rutrum dui tempus at.";
 }
 
-chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-  main();
-});
+window.setTimeout(main, 30);
