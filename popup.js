2ea03c1d3c80a2322a4dc70a6842744d819fcb59
Peter Coles <peter@mrcoles.com>
2018-02-17 15:38:10 -0500

    initial commit

--- popup.js
+++ popup.js
@@ -1,39 +1,13 @@
-function main(tab) {
-  _log("chrome.tabs.executeScript(..., page.js)");
-  chrome.tabs.executeScript(tab.Id, { file: "page.js" }, function() {
-    _log("chrome.tabs.executeScript callback!");
-    var lastError = chrome.runtime.lastError;
+const $ = id => document.getElementById(id);
 
-    if (lastError) {
-      _log("chrome.runtime.lastError: " + lastError.message);
-    } else {
-      _log("chrome.tabs.sendMessage(..., test)");
-      chrome.tabs.sendMessage(tab.id, { msg: "test" }, function() {
-        _log("chrome.tabs.sendMessage callback!");
-        var lastError = chrome.runtime.lastError;
-        if (lastError) {
-          _log("chrome.runtime.lastError: " + lastError.message);
-        } else {
-          console.log("sendMessage got response!!");
-        }
-      });
-    }
-  });
+function main() {
+  $("old-content").style.display = "none";
+  let nc = $("new-content");
+  nc.style.display = "block";
+  nc.innerText =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus auctor accumsan tincidunt. Fusce odio dui, ultricies sit amet ipsum a, ultrices ultrices ligula. In hac habitasse platea dictumst. Aliquam ullamcorper tempor ligula, in feugiat elit porta sed. Cras quis tincidunt lorem, vel pulvinar diam. Sed justo nulla, sodales accumsan rutrum vel, dictum in libero. Morbi pulvinar sapien nulla, eu mattis arcu mollis in. In vehicula lectus mi, sit amet rutrum dui tempus at.";
 }
 
-function _log(msg) {
-  var elt = document.createElement("li");
-  elt.innerText = msg;
-  document.getElementById("msgs").appendChild(elt);
-}
-
-window.addEventListener(
-  "load",
-  function() {
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      var tab = tabs[0];
-      main(tab);
-    });
-  },
-  false
-);
+chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+  main();
+});
