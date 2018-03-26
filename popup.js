7afc3b454b7922dbec44e2031f09fc10d024b876
Peter Coles <peter@mrcoles.com>
2018-03-26 12:31:09 -0400

    Revert "simplify test to just use a timeout"
    This reverts commit a2a43b797f7745b8adff2d425a8bcbc9d7a2da72.

--- popup.js
+++ popup.js
@@ -8,4 +8,6 @@ function main() {
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus auctor accumsan tincidunt. Fusce odio dui, ultricies sit amet ipsum a, ultrices ultrices ligula. In hac habitasse platea dictumst. Aliquam ullamcorper tempor ligula, in feugiat elit porta sed. Cras quis tincidunt lorem, vel pulvinar diam. Sed justo nulla, sodales accumsan rutrum vel, dictum in libero. Morbi pulvinar sapien nulla, eu mattis arcu mollis in. In vehicula lectus mi, sit amet rutrum dui tempus at.";
 }
 
-window.setTimeout(main, 30);
+chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+  main();
+});
